48d3492bbcec6019d5e59c7a0af426c31c4d4d6e
Nesbox314 <46055626+Nesbox314@users.noreply.github.com>
2019-09-20 22:44:43 -0300

    Desenvolvendo tela de login

--- php/efetuaLogin.php
+++ php/efetuaLogin.php
@@ -4,14 +4,29 @@ include 'conexao.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
  
-$sql = "SELECT * FROM usuarios";
+$sql = "SELECT * FROM usuarios WHERE username = '$username' AND password = '$password'";
 
-$sqlTeste = mysqli_query($conn, $sql);
+$sqlVerifica = mysqli_query($conn, $sql);
 
-//if (mysqli_query($conn, $sql)) {
-while($exibe = mysql_fetch_assoc($sqlTeste)){
+$exibe = mysqli_fetch_assoc($sqlVerifica);
+
+$value = "testetestetestetesteet";
+
+if($exibe == null){
+      echo "cucabeludo";
+}else{
+    echo "<script>
+      window.alert('Usuário logado com sucesso!');
+      location.href='../index.html';
+    </script>";
+
+    setcookie("TestCookie", $value, time()+3600, "/", "example.com");
+}
+
+/*if (mysqli_query($conn, $sql)) {
+while($exibe = mysqli_fetch_assoc($sqlTeste)){
       echo $exibe['username'] .'<br>';
 }
 
-//}
+}*/
 ?>
