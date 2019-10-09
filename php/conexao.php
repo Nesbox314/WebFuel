22b1eaaa60edbe440be9b6929f51e0e5196454b8
Nesbox314 <ricardopop96@gmail.com>
2019-10-09 00:32:51 -0300

    Iniciando a transição, criando pasta separada e arrastando arquivos

--- php/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $database = "webfuel";
-    $username = "root";
-    $password = "";
-    
-    $conn = mysqli_connect($servername, $username, $password, $database);
-?>
