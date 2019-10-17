784d8519bf3e8cf73433aeb30524d87301e07d9e
Nesbox314 <ricardopop96@gmail.com>
2019-10-17 19:09:39 -0300

    Terminando de arrumar o projeto para retornar ao desenvolvimento

--- WebFuel/routes/index.js
+++ WebFuel/routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/login', function(req, res, next){
+  res.render('login');
+});
+
+router.get('/cadastroUsuario', function(req, res, next){
+  res.render('cadastroUsuario');
+});
+
+function execSQLQuery(sqlQry, res){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    port     :  3306,
+    user     : 'root',
+    password : '',
+    database : 'WebFuel'
+  });
+
+  connection.query(sqlQry, function(error, results, fields){
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+}
+
 module.exports = router;
