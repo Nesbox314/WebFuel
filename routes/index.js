7fc459b1818d3d067c8d390a4696008b6d39b61c
Nesbox314 <ricardopop96@gmail.com>
2019-10-21 19:02:05 -0300

    Tudo pronto para o início da página inicial vindo do banco

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const app = express();         
+const bodyParser = require('body-parser');
+const port = 3005; //porta padrão
+const mysql = require('mysql');
+const path = require('path');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  port     :  3306,
+  user     : 'root',
+  password : '',
+  database : 'webfuel'
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,23 +34,22 @@ router.get('/cadastroUsuario', function(req, res, next){
   res.render('cadastroUsuario');
 });
 
-function execSQLQuery(sqlQry, res){
-  const connection = mysql.createConnection({
-    host     : 'localhost',
-    port     :  3306,
-    user     : 'root',
-    password : '',
-    database : 'WebFuel'
+/*router.get('/postos', function(req, res, next){
+  connection.query('SELECT * FROM postos', function(error, results, fields){
+  console.log(results);
+  res.render('index', {
+    datasetresult: results
+      nome: results.nome,
+      foto: results.foto,
+      avaliacao: results.avaliacao,
+      precoGasolinaComum: results.precoGasolinaComum,
+      precoGasolinaAditivada: results.precoGasolinaAditivada,
+      precoEtanol: results.precoEtanol,
+      precoGnv: results.precoGnv,
+      precoDieselS10: results.precoDieselS10,
+      precoDieselS500: results.precoDieselS500
+    })
   });
-
-  connection.query(sqlQry, function(error, results, fields){
-      if(error) 
-        res.json(error);
-      else
-        res.json(results);
-      connection.end();
-      console.log('executou!');
-  });
-}
+});*/
 
 module.exports = router;
